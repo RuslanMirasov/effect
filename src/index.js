38c8c76493bca7d753344b4855748b9690b0e016
RuslanMirasov <info@mirasov.dev>
2023-09-12 00:42:29 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import './js/refs.js';
-import './js/axios.js';
-import './js/preloader.js';
-import './js/navigation.js';
-import './js/popup.js';
-import './js/form.js';
-import './js/phone-mask.js';
-import './js/scroll-to-block.js';
-import './js/theme-switcher.js';
+import { Gradient } from './js/gradient';
+const gradient = new Gradient();
+gradient.initGradient('#gradient-canvas');
--- src/js/phone-mask.js
+++
@@ -1,6 +0,0 @@
-import Inputmask from 'inputmask';
-const phones = document.querySelectorAll('input[type="tel"]');
-const phoneMask = new Inputmask('+38 (999) 999-99-99');
-phones.forEach(phone => {
-  phoneMask.mask(phone);
-});
